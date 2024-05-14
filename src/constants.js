6cf4a06d9350d1fbbd5ea528dea784f2f2949ffa
Vasanth Chandrasekar <35191203+Vasanth14@users.noreply.github.com>
2024-05-14 23:01:24 +0530

    initital commit

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const LANGUAGE_VERSIONS = {
+  javascript: "18.15.0",
+  rust: "1.68.2"
+};
+
+export const CODE_SNIPPETS = {
+  javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
+  rust: `\nfn main() {\n\tprintln!("Hello, world!");\n}\n`,
+};
